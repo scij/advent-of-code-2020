3a0d1b51124b4b1342fff7c86f7c302da3202ecc
juergen <juergen.schiewe@gmx.de>
2020-12-30 12:30:29 +0100

    day 9 finished

--- src/advent_of_code_2020/door09.clj
+++ src/advent_of_code_2020/door09.clj
@@ -23,9 +23,33 @@
   (check-first-seq (partition (inc preamble-len) 1 numbers))
   )
 
-(defn find-sum [sum numbers]
-
-  )
 (defn read-file [fn]
   (map #(Integer/parseInt %)
        (s/split (slurp fn) #"\n")))
+
+(defn find-sum-from-here [target values]
+  (loop [lwb (first values)
+         upb (first values)
+         val (first values)
+         sum 0
+         new-vals values]
+    (let [n-sum (+ sum val)
+          n-lwb (min val lwb)
+          n-upb (max val upb)]
+      (cond
+        (= n-sum target) [n-lwb n-upb]
+        (> n-sum target) nil
+        (empty? (rest new-vals)) nil
+        (< n-sum target)
+        (recur n-lwb n-upb (second new-vals) n-sum (rest new-vals))
+        )
+      )
+    )
+  )
+
+(defn try-find-sum [target values]
+  (if-let [result (find-sum-from-here target values)]
+    (reduce + result)
+    (recur target (next values))
+    )
+  )
